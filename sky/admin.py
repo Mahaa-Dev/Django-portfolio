658fd30bf2f67850bd3975bd7e03e2fefd180641
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2023-02-08 23:02:56 +0545

    background image and model added

--- sky/admin.py
+++ sky/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PersonalInfo
+
+admin.site.register([
+    PersonalInfo
+])
--- sky/views.py
+++ sky/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
+from .models import PersonalInfo
+
+
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["info"] = PersonalInfo.objects.get(id=1)
+
+        return context
