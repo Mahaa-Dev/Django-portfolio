b92e390c70cb5726ec5da35003d6f583b01c7e1a
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2023-02-16 07:02:03 +0545

    forms updated

--- sky/forms.py
+++ sky/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import Contact
 
 
+# class ContactForm(forms.ModelForm):
+#     class Meta:
+#         model = Contact
+#         fields = (
+#             "name",
+#             "email",
+#             "subject",
+#             "message",
+#         )
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
@@ -11,3 +21,18 @@ class ContactForm(forms.ModelForm):
             "subject",
             "message",
         )
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "Enter your name", "class": "form-control"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Enter your email", "class": "form-control"}),
+            "subject": forms.TextInput(attrs={"placeholder": "Enter the subject", "class": "form-control"}),
+            "message": forms.Textarea(attrs={"placeholder": "Enter your message", "class": "form-control"}),
+        }
+        labels = {
+            "name": "Your Name",
+            "email": "Your Email",
+            "subject": "Subject",
+            "message": "Message",
+        }
+        label_attrs = {
+            "class": "control-label"
+        }
