3acecb3b6f9a5a52dd3f21dec2b5039d089e719f
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2023-02-25 15:25:11 +0545

    Latest Changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,36 @@
-const name = "{{ info.name }}".split(" ");
-const title = "{{ info.title }}".split(" ");
-let i = 0;
-let j = 0;
+// Select all the links in the navbar
+const links = document.querySelectorAll("nav a");
+// Add event listeners to each link
+links.forEach(link => {
+    link.addEventListener("click", function (event) {
+        // Prevent default link behavior
+        event.preventDefault();
 
-function displayName() {
-    if (i < name.length) {
-        document.getElementById("name").innerHTML += name[i] + " ";
-        i++;
-        setTimeout(displayName, 500);
-    }
-}
+        // Get the target element by its ID
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
 
-function displayTitle() {
-    if (j < title.length) {
-        document.getElementById("title").innerHTML += title[j] + " ";
-        j++;
-        setTimeout(displayTitle, 500);
-    }
-}
+        // Get the offset position of the target element
+        const offset = 80; // Change this value to adjust the offset
+        const targetPosition = targetElement.offsetTop - offset;
+
+        // Animate scrolling to the target element
+        window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth"
+        });
+    });
+});
+
+// smooth scroll behaviour
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
